2b65cd0a81e81979b8ce50097c306ee02f1cf717
robin <nixdmn@gmail.com>
2015-05-31 11:40:23 -0500

    moved manager class back to models

--- django_me/mongo_auth/models.py
+++ django_me/mongo_auth/models.py
@@ -15,17 +15,14 @@ except ImportError:
     from django.utils.importlib import import_module
 
 from django_me.utils import make_password, check_password, datetime_now
-from .managers import PermissionManager, MongoUserManager
+
+
+REDIRECT_FIELD_NAME = 'next'
+MONGOENGINE_USER_DOCUMENT = getattr(settings, 'MONGOENGINE_USER_DOCUMENT', 'django_me.mongo_auth.models.User')
 
 __all__ = (
     'get_user_document',
 )
-
-
-MONGOENGINE_USER_DOCUMENT = getattr(settings, 'MONGOENGINE_USER_DOCUMENT', 'django_me.mongo_auth.models.User')
-
-REDIRECT_FIELD_NAME = 'next'
-
 
 def get_user_document():
     """Get the user document class used for authentication.
@@ -38,7 +35,6 @@ def get_user_document():
     dot = name.rindex('.')
     module = import_module(name[:dot])
     return getattr(module, name[dot + 1:])
-
 
 class SiteProfileNotAvailable(Exception):
     pass
@@ -78,6 +74,13 @@ class ContentType(Document):
     def natural_key(self):
         return (self.app_label, self.model)
 
+
+class PermissionManager(models.Manager):
+    def get_by_natural_key(self, codename, app_label, model):
+        return self.get(
+            codename=codename,
+            content_type=ContentType.objects.get_by_natural_key(app_label, model)
+        )
 
 
 class Permission(Document):
@@ -152,6 +155,46 @@ class Group(Document):
 
     def __unicode__(self):
         return self.name
+
+
+class UserManager(models.Manager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a User with the given username, e-mail and password.
+        """
+        now = datetime_now()
+
+        # Normalize the address by lowercasing the domain part of the email
+        # address.
+        try:
+            email_name, domain_part = email.strip().split('@', 1)
+        except ValueError:
+            pass
+        else:
+            email = '@'.join([email_name, domain_part.lower()])
+
+        user = self.model(username=username, email=email, is_staff=False,
+                          is_active=True, is_superuser=False, last_login=now,
+                          date_joined=now)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        u = self.create_user(username, email, password)
+        u.is_staff = True
+        u.is_active = True
+        u.is_superuser = True
+        u.save(using=self._db)
+        return u
+
+    def make_random_password(self, length=10, allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'):
+        "Generates a random password with the given length and given allowed_chars"
+        # Note that default value of allowed_chars does not have "I" or letters
+        # that look like it -- just to avoid confusion.
+        from random import choice
+        return ''.join([choice(allowed_chars) for i in range(length)])
 
 
 class User(Document):
@@ -327,6 +370,69 @@ class User(Document):
             except (ImportError, ImproperlyConfigured):
                 raise SiteProfileNotAvailable
         return self._profile_cache
+
+
+class MongoUserManager(UserManager):
+    """A User manager wich allows the use of MongoEngine documents in Django.
+
+    To use the manager, you must tell django.contrib.auth to use MongoUser as
+    the user model. In you settings.py, you need:
+
+        INSTALLED_APPS = (
+            ...
+            'django.contrib.auth',
+            'django_me.mongo_auth',
+            ...
+        )
+        AUTH_USER_MODEL = 'mongo_auth.MongoUser'
+
+    Django will use the model object to access the custom Manager, which will
+    replace the original queryset with MongoEngine querysets.
+
+    By default, django_me.mongo_auth.models.User will be used to store users. You
+    can specify another document class in MONGOENGINE_USER_DOCUMENT in your
+    settings.py.
+
+    The User Document class has the same requirements as a standard custom user
+    model: https://docs.djangoproject.com/en/dev/topics/auth/customizing/
+
+    In particular, the User Document class must define USERNAME_FIELD and
+    REQUIRED_FIELDS.
+
+    `AUTH_USER_MODEL` has been added in Django 1.5.
+
+    """
+
+    def contribute_to_class(self, model, name):
+        super(MongoUserManager, self).contribute_to_class(model, name)
+        self.dj_model = self.model
+        self.model = get_user_document()
+
+        self.dj_model.USERNAME_FIELD = self.model.USERNAME_FIELD
+        username = models.CharField(_('username'), max_length=30, unique=True)
+        username.contribute_to_class(self.dj_model, self.dj_model.USERNAME_FIELD)
+
+        self.dj_model.REQUIRED_FIELDS = self.model.REQUIRED_FIELDS
+        for name in self.dj_model.REQUIRED_FIELDS:
+            field = models.CharField(_(name), max_length=30)
+            field.contribute_to_class(self.dj_model, name)
+
+    def get(self, *args, **kwargs):
+        try:
+            return self.get_query_set().get(*args, **kwargs)
+        except self.model.DoesNotExist:
+            # ModelBackend expects this exception
+            raise self.dj_model.DoesNotExist
+
+    @property
+    def db(self):
+        raise NotImplementedError
+
+    def get_empty_query_set(self):
+        return self.model.objects.none()
+
+    def get_query_set(self):
+        return self.model.objects
 
 
 class MongoUser(models.Model):
