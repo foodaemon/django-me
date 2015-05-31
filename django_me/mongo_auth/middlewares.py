d8e564622729314b0d14fb02e76bb3469acc57fa
robin <nixdmn@gmail.com>
2015-05-31 11:20:00 -0500

    Initial Commit

---
+++ django_me/mongo_auth/middlewares.py
@@ -0,0 +1,54 @@
+from bson.objectid import ObjectId
+from django.utils.functional import SimpleLazyObject
+from django.contrib.auth import rotate_token, user_logged_in, SESSION_KEY, HASH_SESSION_KEY, BACKEND_SESSION_KEY
+
+from .backends import get_user
+
+
+def _get_user_session_key(request):
+    # This value in the session is always serialized to a string, so we need
+    # to convert it back to Python whenever we access it.
+    if SESSION_KEY in request.session:
+        return ObjectId(request.session[SESSION_KEY])
+
+
+def login(request, user):
+    """
+    Persist a user id and a backend in the request. This way a user doesn't
+    have to reauthenticate on every request. Note that data set during
+    the anonymous session is retained when the user logs in.
+    """
+    session_auth_hash = ''
+    if user is None:
+        user = request.user
+    if hasattr(user, 'get_session_auth_hash'):
+        session_auth_hash = user.get_session_auth_hash()
+
+    if SESSION_KEY in request.session:
+        if _get_user_session_key(request) != user.pk or (
+                session_auth_hash and
+                request.session.get(HASH_SESSION_KEY) != session_auth_hash):
+            # To avoid reusing another user's session, create a new, empty
+            # session if the existing session corresponds to a different
+            # authenticated user.
+            request.session.flush()
+    else:
+        request.session.cycle_key()
+    request.session[SESSION_KEY] = str(user.pk)  # user._meta.pk.value_to_string(user)
+    request.session[BACKEND_SESSION_KEY] = user.backend
+    request.session[HASH_SESSION_KEY] = session_auth_hash
+    if hasattr(request, 'user'):
+        request.user = user
+    rotate_token(request)
+    user_logged_in.send(sender=user.__class__, request=request, user=user)
+
+
+class CustomAuthenticationMiddleware(object):
+    def process_request(self, request):
+        assert hasattr(request, 'session'), (
+            "The Django authentication middleware requires session middleware "
+            "to be installed. Edit your MIDDLEWARE_CLASSES setting to insert "
+            "'django.contrib.sessions.middleware.SessionMiddleware' before "
+            "'django.contrib.auth.middleware.AuthenticationMiddleware'."
+        )
+        request.user = SimpleLazyObject(lambda: get_user(_get_user_session_key(request)))
---
+++ django_me/utils.py
@@ -0,0 +1,32 @@
+from hashlib import md5, sha1
+
+from django.utils.encoding import smart_str
+
+try:
+    # django >= 1.4
+    from django.utils.timezone import now as datetime_now
+except ImportError:
+    from datetime import datetime
+    datetime_now = datetime.now
+
+
+def get_hexdigest(algorithm, salt, raw_password):
+    raw_password, salt = smart_str(raw_password), smart_str(salt)
+    if algorithm == 'md5':
+        return md5(salt + raw_password).hexdigest()
+    elif algorithm == 'sha1':
+        return sha1(salt + raw_password).hexdigest()
+    raise ValueError('Got unknown password algorithm type in password')
+
+
+def check_password(raw_password, password):
+    algo, salt, hash = password.split('$')
+    return hash == get_hexdigest(algo, salt, raw_password)
+
+
+def make_password(raw_password):
+    from random import random
+    algo = 'sha1'
+    salt = get_hexdigest(algo, str(random()), str(random()))[:5]
+    hash = get_hexdigest(algo, salt, raw_password)
+    return '%s$%s$%s' % (algo, salt, hash)
